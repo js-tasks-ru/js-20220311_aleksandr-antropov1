d4554b1446c52a05bb826d9e3a8826f70d48c6ab
Aleksandr Antropov <aleksander.antropov@gmail.com>
2022-03-25 20:27:19 +0300

    Выполнено задание к 4 занятию

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,86 @@
 export default class ColumnChart {
+  chartHeight = 50;
 
+  constructor(options) {
+    this.options = options;
+
+    if (options) {
+      const { data = [], label, link, value, formatHeading } = options;
+
+      this.data = this._getColumnProps(data);
+      this.label = label;
+      this.value = formatHeading ? formatHeading(value) : value;
+      this.link = link;
+    }
+
+    this.render();
+  }
+
+  destroy() {
+    this.remove();
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  getTemplate() {
+    if (!this.options) {
+      return `<div class='column-chart_loading'></div>`;
+    }
+
+    return `<div class="column-chart" style="--chart-height: 50">
+      <div class="column-chart__title">
+        Total ${this.label}
+        ${
+          this.link !== undefined
+            ? `<a href="${this.link}" class="column-chart__link">
+              View all
+            </a>`
+            : ""
+        }
+      </div>
+      <div class="column-chart__container">
+        <div data-element="header" class="column-chart__header">${
+          this.value
+        }</div>
+        <div data-element="body" class="column-chart__chart">
+          ${
+            this.data.length === 0
+              ? `<img src='charts-skeleton.svg' alt='No data' />`
+              : this.data
+                  .map(
+                    ({ value, percent }) =>
+                      `<div style="--value: ${value}" data-tooltip="${percent}"></div>`
+                  )
+                  .join("")
+          }
+        </div>
+      </div>`;
+  }
+
+  render() {
+    const wrapper = document.createElement("div");
+    wrapper.innerHTML = this.getTemplate();
+
+    this.element = wrapper.childNodes[0];
+  }
+
+  update(data) {
+    this.data = data;
+
+    this.render();
+  }
+
+  _getColumnProps(data) {
+    const maxValue = Math.max(...data);
+    const scale = 50 / maxValue;
+
+    return data.map((item) => {
+      return {
+        percent: ((item / maxValue) * 100).toFixed(0) + "%",
+        value: String(Math.floor(item * scale)),
+      };
+    });
+  }
 }
