7d0a6a6661d9feb2a15ce999d06510ae03dec906
Aleksandr Antropov <aleksander.antropov@gmail.com>
2022-04-05 23:53:30 +0300

    Решена задача DoubleSlider

--- 06-events-practice/3-double-slider/index.js
+++ 06-events-practice/3-double-slider/index.js
@@ -1,3 +1,140 @@
 export default class DoubleSlider {
+  subElements = {};
 
+  constructor({
+    min = 0,
+    max = 0,
+    formatValue = (value) => value,
+    selected = {
+      from: min,
+      to: max,
+    },
+  } = {}) {
+    this.min = min;
+    this.max = max;
+    this.delta = max - min;
+    this.selected = selected;
+    this.formatValue = formatValue;
+
+    this.render();
+  }
+
+  get progress() {
+    return {
+      left: ((this.selected.from - this.min) / this.delta) * 100,
+      right: (1 - (this.selected.to - this.min) / this.delta) * 100,
+    };
+  }
+
+  get template() {
+    return `<div class="range-slider">
+    <span data-element="from">${this.formatValue(this.selected.from)}</span>
+    <div class="range-slider__inner">
+      <span class="range-slider__progress" style="left: ${
+        this.progress.left
+      }%; right: ${this.progress.right}%"></span>
+      <span class="range-slider__thumb-left" style="left: ${
+        this.progress.left
+      }%"></span>
+      <span class="range-slider__thumb-right" style="right: ${
+        this.progress.right
+      }%"></span>
+    </div>
+    <span data-element="to">${this.formatValue(this.selected.to)}</span>
+  </div>`;
+  }
+
+  render() {
+    const wrapper = document.createElement("div");
+
+    wrapper.innerHTML = this.template;
+    this.element = wrapper.firstElementChild;
+
+    this.subElements = this.getSubElements();
+
+    this.initEventListeners();
+  }
+
+  initEventListeners() {
+    let handler;
+
+    this.element.addEventListener("pointerdown", (event) => {
+      document.removeEventListener("pointermove", handler);
+
+      const element = event.target;
+      const isLeft = element === this.subElements.left;
+      const isRight = element === this.subElements.right;
+
+      if (isLeft || isRight) {
+        const { left, width } = this.subElements.range.getBoundingClientRect();
+
+        handler = (event) => this.handleMove(isLeft, left, width, event);
+
+        document.addEventListener("pointermove", handler);
+      }
+    });
+
+    document.addEventListener("pointerup", () => {
+      document.removeEventListener("pointermove", handler);
+
+      if (this.element) {
+        this.element.dispatchEvent(
+          new CustomEvent("range-select", { detail: this.selected })
+        );
+      }
+    });
+  }
+
+  handleMove(isLeft, startX, width, event) {
+    const key = isLeft ? "from" : "to";
+    const direction = isLeft ? "left" : "right";
+    const delta = event.clientX - startX;
+    const newPercentage = delta / width;
+
+    let newValue = Math.round(this.delta * newPercentage) + this.min;
+
+    if (newValue < this.min) {
+      newValue = this.min;
+    }
+    if (newValue > this.max) {
+      newValue = this.max;
+    }
+    if (direction === "left" && newValue > this.selected.to) {
+      newValue = this.selected.to;
+    }
+    if (direction === "right" && newValue < this.selected.from) {
+      newValue = this.selected.from;
+    }
+
+    this.selected[key] = newValue;
+
+    this.subElements.progress.style[direction] = this.progress[direction] + "%";
+    this.subElements[direction].style[direction] =
+      this.progress[direction] + "%";
+    this.subElements[key].innerHTML = this.formatValue(this.selected[key]);
+  }
+
+  getSubElements() {
+    const result = {};
+
+    result.range = this.element.querySelector(".range-slider__inner");
+    result.progress = this.element.querySelector(".range-slider__progress");
+    result.left = this.element.querySelector(".range-slider__thumb-left");
+    result.right = this.element.querySelector(".range-slider__thumb-right");
+    result.from = this.element.querySelector("[data-element='from']");
+    result.to = this.element.querySelector("[data-element='to']");
+
+    return result;
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+  }
 }
